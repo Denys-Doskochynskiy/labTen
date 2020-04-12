c92837dc0d3274b256d1c329569a2d1e538bcdc4
Denys-Doskochynskiy <doskochynska83@gmail.com>
2020-04-12 22:09:43 +0300

    Add get and set from my field

--- src/Stadium.py
+++ src/Stadium.py
@@ -1,11 +1,11 @@
 class Stadium:
     price_in_millions_of_dollars = 12.5
 
-    def __init__(self, name=None, count_of_spectators=None, country=None,
+    def __init__(self, name=None, __count_of_spectators__=None, country=None,
                  size_of_field_in_square_meters=None,
                  lighting_power_in_lux=None, year_of_foundation=None):
         self.name = name
-        self.count_of_spectators = count_of_spectators
+        self.count_of_spectators = __count_of_spectators__
         self.country = country
         self.size_of_field_in_square_meters = size_of_field_in_square_meters
         self.lighting_power_in_lux = lighting_power_in_lux
@@ -31,3 +31,46 @@ class Stadium:
     def __del__(self):
         print("Delete " + self.__class__.__name__ + " | Object ID: " + str(id(self)))
         return
+
+#################################################
+    def get_name(self):
+        return self.name
+
+    def set_country(self, name):
+        self.name = name
+
+#################################################
+    def get_country(self):
+        return self.country
+
+    def set_country(self, country):
+        self.country = country
+
+#################################################
+    def get_count_of_spectators(self):
+        return self.count_of_spectators
+
+    def set_count_of_spectators(self, count_of_spectators):
+        self.count_of_spectators = count_of_spectators
+
+#################################################
+    def get_size_of_field_in_square_meters(self):
+        return self.size_of_field_in_square_meters
+
+    def set_size_of_field_in_square_meters(self, size_of_field_in_square_meters):
+        self.size_of_field_in_square_meters = size_of_field_in_square_meters
+
+#################################################
+    def get_lighting_power_in_lux(self):
+        return self.lighting_power_in_lux
+
+    def set_lighting_power_in_lux(self, lighting_power_in_lux):
+        self.lighting_power_in_lux = lighting_power_in_lux
+
+#################################################
+    def get_year_of_foundation(self):
+        return self.year_of_foundation
+
+    def set_year_of_foundation(self, year_of_foundation):
+        self.year_of_foundation = year_of_foundation
+#################################################
--- src/main.py
+++ src/main.py
@@ -3,11 +3,14 @@ from src.Stadium import Stadium
 
 def main():
     lvov = Stadium("Lvov", 15750, "Ua", 190.65, 28.65, 1898)
-    kharkov = Stadium("Kharkov", 12500, "ua", 10500, 16.05, 1990)
-    odessa = Stadium("Odessa", 10000, "Ua", 900.5, 29.75, 2005)
-    print(lvov.__str__())
-    print(kharkov.__str__())
-    print(odessa.__str__())
+    kharkov = Stadium("Kharkov", 12500, "ua")
+    odessa = Stadium()
+    stadium = [lvov, kharkov, odessa]
+    [print(count_of_object) for count_of_object in stadium]
+
+# print(lvov.__str__())
+# print(kharkov.__str__())
+# print(odessa.__str__())
 
 
 if __name__ == "__main__":
